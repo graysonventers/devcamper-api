aac98da1d41be4b67fc56fa7797ef27acdf7726c
Grayson Venters <graysonventers@gmail.com>
2020-09-18 11:56:46 -0700

    Added geocoder, middleware, bootcamp route function, created seeder, etc.

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -4,14 +4,17 @@ const {
     getBootcamp, 
     createBootcamp, 
     updateBootcamp, 
-    deleteBootcamp 
+    deleteBootcamp,
+    getBootcampsInRadius
 } = require('../controllers/bootcamps');
 
 const router = express.Router();
 
+router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
+
 router.route('/')
 .get(getBootcamps)
-.post(createBootcamp);
+.post(createBootcamp)
 
 router.route('/:id')
 .get(getBootcamp)
