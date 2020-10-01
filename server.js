b75b78e51abe906f3a99bfd9d71c6646cad39120
Grayson Venters <graysonventers@gmail.com>
2020-10-01 12:39:35 -0700

    Added readme and starter config

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.use(fileupload());
 app.use(mongoSanitize());
 
 // Set security headers
-app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
 
 // Prevent xss attacks
 app.use(xss());
